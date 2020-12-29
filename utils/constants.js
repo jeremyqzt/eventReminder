5a11c83636a582f72a430356cc24cca64f15822f
jeremyqzt <jq357@hotmail.com>
2020-12-28 19:37:33 -0500

    add enums

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+const EventEnum = Object.freeze({
+  Birthday: 1,
+  Aniversary: 2,
+  SharedHoliday: 3,
+  Other: 4,
+});
+
+const EventDateTypeEnum = Object.freeze({
+  Solar: 1,
+  Lunar: 2,
+});
