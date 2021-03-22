40abd5e7a04a91d39b807b5e9dbb9578e4299179
jeremyqzt <jq357@hotmail.com>
2021-03-21 20:57:51 -0400

    adding utils

---
+++ utils/eventUtils.js
@@ -0,0 +1,10 @@
+/* 
+Prototype for the event
+{
+    date: string
+    eventType: EventType (Define This)
+    dateType: DateType (I.e. lunar, Define this)
+    eventName: string
+    equivalentSolarDate: dateTime
+}
+*/
