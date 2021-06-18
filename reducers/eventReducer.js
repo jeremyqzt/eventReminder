c63e112a66617bb0df7cd79c13fe718187557143
jeremyqzt <jq357@hotmail.com>
2021-06-17 20:56:01 -0400

    reducer

---
+++ reducers/eventReducer.js
@@ -0,0 +1,103 @@
+import "react-native-get-random-values";
+import { combineReducers } from "redux";
+
+const initial = {
+  event: {
+    id: "event1",
+    date: "2021-01-01",
+    eventName: "",
+    description: "",
+    notes: [],
+    reoccurs: true,
+    notify: false,
+    syncCalender: false,
+    contacts: [],
+  },
+};
+
+const initialIds = ["event1"];
+
+const addOrEditEventEntry = (state, action) => {
+  const { event } = action;
+  const {
+    id,
+    date,
+    eventName,
+    description,
+    notes,
+    reoccurs,
+    notify,
+    syncCalender,
+    contacts,
+  } = event;
+
+  const newEvent = {
+    id: id,
+    date,
+    eventName,
+    description,
+    notes,
+    reoccurs,
+    notify,
+    syncCalender,
+    contacts,
+  };
+
+  return {
+    ...state,
+    [id]: newEvent,
+  };
+};
+
+const removeEventEntry = (state, action) => {
+  const { toRemove } = action;
+
+  const newState = {
+    ...state,
+  };
+
+  delete newState[toRemove];
+
+  return newState;
+};
+
+const allIds = (state = initialIds, action) => {
+  switch (action.type) {
+    case "ADD_EVENT": {
+      return state.concat(action.contact.id);
+    }
+    case "UPDATE_EVENT": {
+      return state;
+    }
+    case "DELETE_EVENT": {
+      return state.filter((item) => item !== action.toRemove);
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const byId = (state = initial, action) => {
+  switch (action.type) {
+    case "ADD_EVENT": {
+      return addOrEditEventEntry(state, action);
+    }
+    case "UPDATE_EVENT": {
+      return addOrEditEventEntry(state, action);
+    }
+    case "DELETE_EVENT": {
+      return removeEventEntry(state, action);
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const eventReducer = combineReducers({
+  allIds: allIds,
+  byId: byId,
+});
+
+export default eventReducer;
