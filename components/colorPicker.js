550ca2ccf8807752303325fc43dca6fc2191486c
jeremyqzt <jq357@hotmail.com>
2021-06-20 11:20:54 -0400

    starting color picker

---
+++ components/colorPicker.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { ListItem, Text, Input, Icon, Button } from "react-native-elements";
+import { View } from "react-native";
+import { styles } from "./styles";
+import { DefaultTheme } from "../utils/constants";
+
+const ColorPicker = (props) => {
+  const [color, setColor] = useState("white");
+
+  return <View></View>;
+};
+
+export default ColorPicker;
