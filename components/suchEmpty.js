db1fbf26a292390feae5272045dfa8e703434e21
jeremyqzt <jq357@hotmail.com>
2021-07-12 21:11:25 -0400

    start such empty page

---
+++ components/suchEmpty.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+import tailwind from "tailwind-rn";
+
+const SuchEmptyWow = (props) => {
+  return <View></View>;
+};
+
+export default SuchEmptyWow;
