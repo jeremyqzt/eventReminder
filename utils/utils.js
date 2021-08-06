79a157e9c2aaf630db4a5d6a65bd7ed712f46831
jeremyqzt <jq357@hotmail.com>
2021-08-05 20:13:38 -0400

    remove unused func

--- utils/utils.js
+++ utils/utils.js
@@ -116,50 +116,6 @@ export const getDifferenceFromToday = (date) => {
   return -1;
 };
 
-export const GetNextOccurence = (date, reoccurType) => {
-  const today = new Date();
-
-  if (today === date) {
-    return 0;
-  }
-
-  if (date > today) {
-    const diffTime = Math.abs(date - today);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
-  }
-
-  let nextEvent;
-  switch (reoccurType) {
-    case AvailableReoccurences[0].value: {
-      //impossible
-      break;
-    }
-    case AvailableReoccurences[1].value: {
-      //Monthly
-      nextEvent = new Date(
-        today.getFullYear(),
-        today.getMonth() + 1,
-        today.getDate()
-      );
-      break;
-    }
-    case AvailableReoccurences[2].value: {
-      //Yearly
-      nextEvent = new Date(
-        today.getFullYear() + 1,
-        today.getMonth(),
-        today.getDate()
-      );
-      break;
-    }
-  }
-
-  const diffTime = Math.abs(nextEvent - today);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  return diffDays;
-};
-
 export const getEvents = () => {
   return EventEnum;
 };
