58f847a25327208aeb942ac0b88895b0adb2bf85
jeremyqzt <jq357@hotmail.com>
2021-09-05 20:34:54 -0400

    create date key

--- utils/utils.js
+++ utils/utils.js
@@ -43,6 +43,18 @@ export const formatDate = (date) => {
   return date.toLocaleDateString("en-US", options);
 };
 
+export const createDateKey = (date) => {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+};
+
 const getNextMonthOccurence = (date, today) => {
   const wouldBeThisMonth = new Date(
     today.getFullYear(),
